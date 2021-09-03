6aab106f29dd3a723a5f1eda52147ae059214bff
Vu_Hong_Son <sonvuhong177@gmail.com>
2021-09-03 16:59:25 +0700

    login successfully

--- src/redux/actions/auth.actions.js
+++ src/redux/actions/auth.actions.js
@@ -2,10 +2,11 @@ import * as types from "../constants/auth.constants";
 import { toast } from "react-toastify";
 import api from "../../apiService";
 
+const LOCAL_BE_URL = "http://localhost:5000";
+
 const register = (formData) => async (dispatch) => {
   console.log("register info:", formData);
   dispatch({ type: types.REGISTER_REQUEST });
-  const LOCAL_BE_URL = "http://localhost:5000";
 
   try {
     const data = await api.post(`${LOCAL_BE_URL}/user/create`, formData);
@@ -18,11 +19,14 @@ const register = (formData) => async (dispatch) => {
 };
 
 const login = (formData) => async (dispatch) => {
-  console.log("hahaha", formData);
+  console.log("login info", formData);
   dispatch({ type: types.LOGIN_REQUEST });
 
   try {
-    const data = await api.post("/auth/login", formData);
+    const data = await api.post(
+      `${LOCAL_BE_URL}/auth/loginWithEmail`,
+      formData
+    );
     console.log(data);
     dispatch({ type: types.LOGIN_SUCCESS, payload: data });
   } catch (error) {
